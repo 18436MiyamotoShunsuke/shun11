4f935a9d7f154f037336256964adcabe2716e40d
Shunsuke Miyamoto <18436@toba-cmt.ac.jp>
2021-10-20 16:06:36 +0900

    説明を追加

--- tcp_server.py
+++ tcp_server.py
@@ -1,14 +1,21 @@
 import socket
-
+#AF=IPv4という意味
+#TCP/IPの場合はSOCK_STREAMを使う
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM)as s:
+    #.IPアドレスとポートを指定
     s.bind(('127.0.0.1',50007))
+    # 1 接続
     s.listen(1)
+    # connection するまで待つ
     while True:
+    # 誰かがアクセスしてきたら、コネクションとアドレスを入れる
        conn,addr=s.accept()
        with conn:
          while True:
+             #データを受け取る
             data =conn.recv(1024)
             if not data:
                 break
             print('data :{},addr:{}'.format(data,addr))
+            #クライアントにデータを返す(b->byteでないといけない)
             conn.sendall(b'Received: '+data)
