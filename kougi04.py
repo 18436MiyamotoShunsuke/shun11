6fe1aafbfbbf27ce400a55a6782458eed80e914a
Shunsuke Miyamoto <18436@toba-cmt.ac.jp>
2021-10-27 15:23:43 +0900

    リストの練習を追記

---
+++ kougi04.py
@@ -0,0 +1,13 @@
+sales=[13,3523,31,238]
+sum=0
+for sale in sales:
+    sum+=sale
+else:
+    print(sum)
+
+for i in range(10):
+    print(i)
+for i in range(10):
+    print(i)
+    if i ==3:
+        break
